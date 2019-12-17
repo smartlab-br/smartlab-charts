62c85ee7df1f2a01e611ef2143a2ecfd63dae203
Rodrigo Fagundes <rmfagundes@gmail.com>
2019-12-17 10:39:57 -0300

    Setting up QA gates.

---
+++ lint/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
